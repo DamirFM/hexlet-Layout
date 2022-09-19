957dca93bf3e517b6f030b6cfdd6d68ca4db3a61
Damir Yakupov <yakupovdamir93@gmail.com>
2022-09-18 23:35:11 -0400

    add index.js

---
+++ lesson/index.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <title>Основной файл index.html</title>
+
+    <style>
+      p {
+        color: red;
+        font-size: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <p>Параграф с размером шрифта 20 пикселей. Текст написан красным цветом.</p>
+  </body>
+</html>
