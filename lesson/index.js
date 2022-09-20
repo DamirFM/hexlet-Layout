911273f2d560f62385b516317bcd890cf40ecfbc
Damir Yakupov <yakupovdamir93@gmail.com>
2022-09-19 23:34:01 -0400

    add index.js

--- lesson/index.js
+++ lesson/index.js
@@ -3,15 +3,10 @@
   <head>
     <meta charset="UTF-8">
     <title>Основной файл index.html</title>
-
-    <style>
-      p {
-        color: red;
-        font-size: 20px;
-      }
-    </style>
   </head>
   <body>
-    <p>Параграф с размером шрифта 20 пикселей. Текст написан красным цветом.</p>
+    <p style="color: red; font-size: 20px;">
+      Параграф с размером шрифта 20 пикселей. Текст написан красным цветом.
+    </p>
   </body>
 </html>
